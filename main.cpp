f1253ee2e309b8873e18f11d4a37cb4cc1685b54
jhw2167 <jhw2167@columbia.edu>
2022-08-18 18:09:56 -0500

    added boost package for general algs, basic app opens photos foder and reads name of all .jpg and .png files

--- main.cpp
+++ main.cpp
@@ -1,25 +1,54 @@
 /* Photo Reader main.cpp */
 #include "main.h"
 
+const unordered_set<string> VALID_TYPES{ ".png", ".jpg" };
+
+
+bool validFile(fs::path file) {
+    string ext = boost::algorithm::to_lower_copy(file.extension().u8string());
+    bool ext_exists = VALID_TYPES.find(ext)!=VALID_TYPES.end();
+    return !fs::is_directory(file) && ext_exists;
+}
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+    //Open a Directory with all images
+    fs::path current = fs::current_path();
+    fs::path path{ current.u8string() +"\\photos" };
+    if (!fs::exists(path) || !fs::is_directory(path)) {
+        //Exit
+        string msg = string("") + "No 'photos' folder found, please add a folder " +
+            "called " + path.u8string() + " with your images to the directory with " +
+            "this program. ";
+            cout << msg;
+            return -1;
     }
+    
+    //Get list of all image names
+    vector<string> files;
+    for (const auto& entry : fs::directory_iterator(path))
+    {
+        fs::path filename = entry.path().filename();
+        if (!validFile(filename)) {
+            cout << "File: " << filename << " is not an image\n";
+            continue;
+        }
+        
+        sf::Image img;
+        if (!img.loadFromFile(entry.path().string())) {
+            cout << "File: " << filename << " could not be loaded\n";
+            continue;
+        }
+    }
+
+    //While list !empty, iterate
+    while (!files.empty()) {
+
+    }
+
+    //Open, read image, write image as JSON, next
+
+    //close and exit
 
     return 0;
 }
--- main.h
+++ main.h
@@ -1,7 +1,69 @@
 #pragma once
+
+
+//Precompiled Header for Project
+#pragma once
+
+//SFML includes
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
 #include <SFML/System.hpp>
+
+/*  Std lib includes  */
+
+//containers
+#include <string>
+#include <array>
+#include <vector>
+#include <list>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <functional>
+
+//Threads
+#include <thread>
+#include <mutex>
+
+//files
+#include <filesystem>
+
+//stream ops
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <ostream>
+
+	//Math ops
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+#include <cstdlib>
+#include <random>
+
+	//Exception ops
+#include <exception>
+#include <stdexcept>
+
+//boost
+#include <boost/algorithm/string.hpp>
+
+//Other
+#include <ctime>
+#include <memory>
+
+//JSON
+#include <nlohmann/json.hpp>
+
+
+//Project includes
+
+
+//Usings
+using namespace std;
+namespace fs = std::filesystem;
